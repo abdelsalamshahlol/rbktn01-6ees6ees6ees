58ddb13078e12b406c6fe30fad13f184addeaa62
abdulsalam shahlol <abdulsalamshahlol@gmail.com>
2019-11-26 17:54:36 +0100

    RetiredForagerBee class complete

--- rbk7-6ees6ees6ees/src/ForagerBee.js
+++ rbk7-6ees6ees6ees/src/ForagerBee.js
@@ -1,3 +1,13 @@
-class ForagerBee {
-  // TODO..
+class ForagerBee extends HoneyMakerBee {
+  constructor(){
+  	super();
+  	this.age = 10;
+  	this.job = 'find pollen';
+  	this.canFly = true;
+  	this.treasureChest = new Array();
+  }
+
+  forage(treasue) {
+  	this.treasureChest.push(treasue);
+  }
 };
